77f42e5d63ce7a8545f44d587f0c2e3661777a5d
John R. Frank <jrf@diffeo.com>
2013-10-24 11:34:07 +0000

    beginning of tests

---
+++ py/src/tests/streamcorpus_filters/test_streamcorpus_filters.py
@@ -0,0 +1,9 @@
+
+
+from streamcorpus_filters import Filter
+
+def test_streamcorpus_filters():
+    filter = Filter()
+
+    path_to_thrift_message = 
+    filter.load_filter_names(path_to_thrift_message)
