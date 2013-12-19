76e498eb7f0aa870ea9b2c8a86a121cf40fcf25c
Leonid Volnitsky <Leonid@Volnitsky.com>
2013-12-19 21:51:07 +0200

    test_throughput_speed: if file name is '-' then use standard input (for 1st arg) or output (for 2nd argument)
    // this is equivalent
    test_throughput_speed streamcorpus/test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc  /tmp/tts.out
    test_throughput_speed - - <streamcorpus/test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc  >/tmp/tts.out
    cat streamcorpus/test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc | test_throughput_speed  - >(cat > /tmp/tts.out)
    test_throughput_speed <(cat streamcorpus/test-data/john-smith-tagged-by-lingpipe-0-v0_3_0.sc)  >(cat > /tmp/tts.out)

--- cpp/test_throughput_speed.cc
+++ cpp/test_throughput_speed.cc
@@ -36,15 +36,15 @@ int main(int argc, char **argv) {
 
 	//////////////////////////////////////////////////////////////////////////////////  CONFIG
 
-	constexpr long max_items = 1000; 		cout << "max-items: " << max_items << endl;
-	cout << std::fixed;
+	constexpr long max_items = 1000; 		cerr << "max-items: " << max_items << endl;
+	cerr << std::fixed;
 	
 	////////////////////////////////////////////////////////////////////////////////// FILES
 
-        cout << "input  file: " << argv[1] << endl;
-        cout << "output file: " << argv[2] << endl;
-	int input_fh  = open (argv[1], O_RDONLY); 	if (input_fh  == -1) { cerr << "error: can't open " << argv[1] << endl; exit(2); } ;
-	int output_fh = creat(argv[2], 664);	 	if (output_fh == -1) { cerr << "error: can't open " << argv[2] << endl; exit(3); } ;
+        cerr << "input  file: " << argv[1] << endl;
+        cerr << "output file: " << argv[2] << endl;
+	int input_fh  = *argv[1]=='-'  ?  0 : open (argv[1], O_RDONLY); 	if (input_fh  == -1) { cerr << "error: can't open " << argv[1] << endl; exit(2); } ;
+	int output_fh = *argv[2]=='-'  ?  1 : creat(argv[2], 664);     	if (output_fh == -1) { cerr << "error: can't open " << argv[2] << endl; exit(3); } ;
 
 	/////////////////////////////////////////////////////////////////////////////////// TRANSPORT / PROTOCOL
 	
@@ -75,10 +75,10 @@ int main(int argc, char **argv) {
 			auto diff  = chrono::high_resolution_clock ::now() - start;
 			double sec   = chrono::duration_cast<chrono::nanoseconds>(diff).count()/1e9;
 
-			cout << "\nDE-SERIALIZATION\n";
-			cout << "test_objects: " << test_objects.size() << endl;
-			cout << "time: "         << sec << " sec\n";
-			cout << "objects/sec: "  << test_objects.size() / sec << endl;
+			cerr << "\nDE-SERIALIZATION\n";
+			cerr << "test_objects: " << test_objects.size() << endl;
+			cerr << "time: "         << sec << " sec\n";
+			cerr << "objects/sec: "  << test_objects.size() / sec << endl;
 			}
 
 	///////////////////////////////////////////////////////////////////////////////  SERIALIZATION 
@@ -97,11 +97,10 @@ int main(int argc, char **argv) {
 			auto diff  = chrono::high_resolution_clock ::now() - start;
 			double sec   = chrono::duration_cast<chrono::nanoseconds>(diff).count()/1e9;
 
-			cout << "\nSERIALIZATION\n";
-			cout << "test_objects written: " << write_count << endl;
-			cout << "test_objects: " << test_objects.size() << endl;
-			cout << "time: "      << sec << " sec" << endl;
-			cout << "objects/sec: " << test_objects.size() / sec << endl;
+			cerr << "\nSERIALIZATION\n";
+			cerr << "test_objects written: " << write_count << endl;
+			cerr << "test_objects: " << test_objects.size() << endl;
+			cerr << "time: "      << sec << " sec" << endl;
+			cerr << "objects/sec: " << test_objects.size() / sec << endl;
 			}
 }
-
